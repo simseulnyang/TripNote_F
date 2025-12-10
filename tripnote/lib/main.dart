bc1b6360db531ab5a41670e9a69a9ed94003c6fc
simseulnyang <happysseul627@gmail.com>
2025-12-10 15:09:55 +0900

    ✨Feat : 기본 로케일 한국어 설정

--- tripnote/lib/main.dart
+++ tripnote/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'core/config/env_config.dart';
 import 'app.dart';
@@ -18,6 +20,10 @@ void main() async {
 
   // 환경변수 로드
   await dotenv.load(fileName: 'assets/.env');
+
+  // 기본 로케일 설정(한국어)
+  Intl.defaultLocale = 'ko_KR';
+  await initializeDateFormatting();
 
   // 카카오 SDK 초기화
   KakaoSdk.init(
